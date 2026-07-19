e311536c5311f8ddb86c943508349e8f769d94cd
Hemasundar2006 <marothihemasundar03@gmail.com>
2026-07-19 16:16:20 +0530

    feat: implement multer middleware for file upload handling

--- src/middlewares/uploadMiddleware.js
+++ src/middlewares/uploadMiddleware.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.uploadToCloudinary = exports.upload = void 0;
+exports.uploadToCloudinary = exports.uploadAppFile = exports.upload = void 0;
 const multer_1 = __importDefault(require("multer"));
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
